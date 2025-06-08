daffbb97e84f158cd34a1a6332759746b4970722
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2025-06-08 22:34:55 +0530

    chore: add deps of cors

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,14 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://ai-proctored-system.vercel.app",
+    origin: [
+      "https://ai-proctored-system.vercel.app",
+      "http://localhost:3000",
+      "http://localhost:5000",
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.urlencoded({ extended: true }));
