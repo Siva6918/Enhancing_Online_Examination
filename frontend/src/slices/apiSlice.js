cfd4ffd851c55f591bea2a11db0a15f5dcd66d8a
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2025-06-08 22:06:19 +0530

    fix: fix headers

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -6,7 +6,13 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.REACT_APP_BACKEND_URL,
     credentials: 'include',
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem('token');
+      headers.set('Authorization', `Bearer ${token}`);
+      return headers;
+    },
   }),
+
   tagTypes: ['User'],
   // it like a prent to other api
   // it a build in builder
