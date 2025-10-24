fa5cea9daedd5568343447f2dfa249573742b995
siva918 <vasanthavenkatasiva@gmail.com>
2025-10-25 02:19:15 +0530

    Fix client-side routing for Vercel

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Suspense>
-    <>
+  <Suspense fallback={<div>Loading...</div>}>
+    <BrowserRouter>
       <App />
-    </>
-  </Suspense>,
+    </BrowserRouter>
+  </Suspense>
 );
